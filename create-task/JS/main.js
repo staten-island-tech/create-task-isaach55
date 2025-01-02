d54e870a9db7aa0a93731786332e19fcbdc376c4
isaach55 <isaach55@nycstudents.net>
2025-01-02 12:30:33 -0500

    make all vectors need same length

--- create-task/JS/main.js
+++ create-task/JS/main.js
@@ -7,28 +7,50 @@ const DOMSelectors = {
   vectorInputContainer: document.getElementById("vectorInputContainer"),
   vectorInput: document.getElementById("vectorInput"),
   submitVector: document.getElementById("submit"),
+  clearButton: document.getElementById("clearButton"),
   startButton: document.getElementById("submitButton"),
 };
 
 let vectorArray = [];
 console.log(vectorArray);
 
-DOMSelectors.submitVector.addEventListener("click", (event) => {
-  event.preventDefault();
+DOMSelectors.submitVector.addEventListener("click", (submit) => {
+  if (vectorArray.length == 0) {
+    submit.preventDefault();
+    let vectorString = DOMSelectors.vectorInput.value;
+    console.log(vectorString);
+    let vectorJS = JSON.parse(vectorString); //User inputs as a string, converts into javascript
+    console.log(vectorJS);
+    vectorArray.push(vectorJS);
+    console.log(vectorArray);
+    console.log("max length =", vectorJS.length);
+    DOMSelectors.vectorInput.value = "";
+  }
+});
+
+/*   submit.preventDefault();
   let vectorString = DOMSelectors.vectorInput.value;
   console.log(vectorString);
   let vectorJS = JSON.parse(vectorString); //User inputs as a string, converts into javascript
   console.log(vectorJS);
   vectorArray.push(vectorJS);
   console.log(vectorArray);
-  DOMSelectors.vectorInput.value = "";
+  if (vectorArray.length == 1) {
+    let vectorLength = vectorJS.length;
+    console.log("max length: ", vectorLength);
+  }
+  DOMSelectors.vectorInput.value = "";*/
+
+DOMSelectors.clearButton.addEventListener("click", (clearVectors) => {
+  vectorArray = [];
+  console.log(vectorArray);
 });
 
 //ADD VECTORS 1 at a time, number of vectors needs to bne equal to dimension of vector, check dimension of vector by iterating thru values until  breaking when no value
 
 //submit button iterates and makes sure length of all values is same YIPPIEEE
 
-function createVectorArray() {}
+function createVectorArray(vectors) {}
 
 function dotProduct(vector1, vector2) {
   if (vector1.length == vector2.length) {
